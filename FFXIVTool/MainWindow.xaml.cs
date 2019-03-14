18aa6fc5856de9d4df9d87244845044bb54a4ca2
James Stine <leon.blade@gmail.com>
2019-03-14 19:24:27 -0400

    Change title, update version and fallback values to aot toggle.

--- FFXIVTool/MainWindow.xaml.cs
+++ FFXIVTool/MainWindow.xaml.cs
@@ -120,7 +120,7 @@ namespace FFXIVTool
         }
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            this.Title = "FFXIV Screenshot Tool-v" + version + "-Made By:Johto&LeonBlade";
+            this.Title = "SSTool v" + version + " By: Johto & LeonBlade";
             DataContext = new MainViewModel();
             var accentColor = Properties.Settings.Default.Accent;
             new PaletteHelper().ReplaceAccentColor(accentColor);
