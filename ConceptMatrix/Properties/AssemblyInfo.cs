74ac1aa273228d829039a7622f54880ccfefd915
Ani <56663405+Luminiari@users.noreply.github.com>
2020-08-14 19:05:39 +1000

    Version control update
    Version updated to 2.0.9.13. Copyright info updated to 2020.

--- ConceptMatrix/Properties/AssemblyInfo.cs
+++ ConceptMatrix/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConceptMatrix")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.9.12")]
-[assembly: AssemblyFileVersion("2.0.9.12")]
+[assembly: AssemblyVersion("2.0.9.13")]
+[assembly: AssemblyFileVersion("2.0.9.13")]
