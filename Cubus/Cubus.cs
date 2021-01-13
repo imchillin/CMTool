09e7d9e9c3b4a311414acf06f6d9166c4973f6e8
James Stine <leon.blade@gmail.com>
2021-01-12 23:49:24 -0500

    Cubus test

---
+++ Cubus/Cubus.cs
@@ -0,0 +1,26 @@
+﻿using Reloaded.Hooks;
+using Reloaded.Hooks.Definitions;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Cubus
+{
+	public static class Cubus
+	{
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+		private delegate void PlayAnimation(IntPtr ptr, short animId, short loopId, IntPtr ptr2);
+
+		private static IHook<PlayAnimation> playAnimationHook;
+
+		[DllExport]
+		public static void Initialize()
+		{
+			playAnimationHook = ReloadedHooks.Instance.CreateHook<PlayAnimation>(Animation, 0x7FF7955134D0);
+		}
+
+		public static void Animation(IntPtr ptr, short animId, short loopId, IntPtr ptr2)
+		{
+			playAnimationHook.OriginalFunction(ptr, 0, 979, ptr2);
+		}
+	}
+}
